30dcb842cc71a61a0aad63530e499e55c34ab6d1
jordandelaney <jordan.delaney@me.com>
2025-02-25 01:41:03 -0500

    if last day of month then run script

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from models import API, MailMan
+from datetime import datetime as dt
+import calendar
 
-api = API()
-mailman = MailMan()
+today = dt.today()
+last_day_of_month = calendar.monthrange(today.year, today.month)[1]
 
-charge_cost = api.get_charge_cost()
-if charge_cost:
-    msg = f"Your total charge expenses for the month are ${charge_cost}"
-    mailman.send_mail(msg)
+if today.day == last_day_of_month:
+    api = API()
+    mailman = MailMan()
+
+    charge_cost = api.get_charge_cost()
+    if charge_cost:
+        msg = f"Your total charge expenses for the month are ${charge_cost}"
+        mailman.send_mail(msg)
 
 
